7b81639c5a5b7f74a5505ad4db98b42b4476ae26
kamil-studiograficzne <40562184+kamil-studiograficzne@users.noreply.github.com>
2018-06-27 11:36:15 +0200

    zrobiony desktop

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
    });
+
+   var coll = document.getElementsByClassName("collapsible");
+   var i;
+   
+   for (i = 0; i < coll.length; i++) {
+     coll[i].addEventListener("click", function() {
+       this.classList.toggle("unfold");
+       var content = this.nextElementSibling;
+       if (content.style.maxHeight){
+         content.style.maxHeight = null;
+       } else {
+         content.style.maxHeight = content.scrollHeight + "px";
+       } 
+     });
+   }
+
+   
